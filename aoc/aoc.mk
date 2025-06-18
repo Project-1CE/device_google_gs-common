5243fe59f77f529924044c761abba21b9ad992cb
Michael Bestas <mkbestas@gmail.com>
2025-06-19 02:37:57 +0300

    gs-common: Remove non-existent vendor includes
    Change-Id: I1bc76430d30259984f4eb91907be61ef39646c27

--- aoc/aoc.mk
+++ aoc/aoc.mk
@@ -5,15 +5,5 @@ PRODUCT_PACKAGES += dump_aoc \
 		    aocxd
 
 # If AoC Daemon is not present on this build, load firmware at boot via rc
-ifeq ($(wildcard vendor/google/whitechapel/aoc/aocd),)
-PRODUCT_COPY_FILES += \
-	device/google/gs-common/aoc/conf/init.aoc.nodaemon.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.aoc.rc
-else
 PRODUCT_COPY_FILES += \
 	device/google/gs-common/aoc/conf/init.aoc.daemon.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.aoc.rc
-endif
-
-ifneq ($(wildcard vendor/google/whitechapel/aoc/aocx/aidl/aocx/framework_compatibility_matrix.xml),)
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    vendor/google/whitechapel/aoc/aocx/aidl/aocx/framework_compatibility_matrix.xml
-endif
