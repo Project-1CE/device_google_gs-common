900f83a12d29353fced2fc8de480ca5db343cf9a
Michael Bestas <mkbestas@gmail.com>
2025-06-19 02:17:37 +0300

    gs-common: Remove userdebug/eng configuration
    Change-Id: Ie1e8b6bbebdfac97f8bc382e41c70cc8a32caf07

--- gyotaku_app/gyotaku_fingerprint.mk
+++
@@ -1,5 +0,0 @@
-# Specific build for fingerprint
-PRODUCT_PACKAGES_DEBUG += \
-   Gyotaku
-
-BOARD_SEPOLICY_DIRS += device/google/gs-common/gyotaku_app/fingerprint
--- ramdump_and_coredump/ramdump_and_coredump.mk
+++ ramdump_and_coredump/ramdump_and_coredump.mk
@@ -1,14 +1,9 @@
 PRODUCT_PACKAGES += \
   sscoredump \
 
-PRODUCT_PACKAGES_DEBUG += \
-    dump_ramdump \
-    ramdump \
-
 # When neither AOSP nor factory targets
 ifeq (,$(filter aosp_% factory_%, $(TARGET_PRODUCT)))
   PRODUCT_PACKAGES += SSRestartDetector
-  PRODUCT_PACKAGES_DEBUG += RamdumpUploader
 endif
 
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/ramdump_and_coredump/sepolicy
